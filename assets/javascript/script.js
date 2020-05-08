44e0c0a6936cf358170ba7116af7e98a4776afe6
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-05-07 20:06:29 -0500

    added holiday API and got it displaying a specific holiday

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // Kanye Quote API
     function kanyeQuoteDisplay() {
 
         var queryURL = "https://api.kanye.rest";
@@ -9,7 +10,6 @@ $(document).ready(function () {
         method: "GET"
         })
         .then(function(response) {
-            console.log(response);
 
             // Creates paragraph with quote and appends to div
             let kanyeQuote = $("<p>").text(response.quote);
@@ -29,8 +29,33 @@ $(document).ready(function () {
 
     setInterval(updateClock, 1000);
 
+    function USHolidaysDisplay() {
+
+        let APIKey = "29671703895b844822f5b4b4b459925e35ceadde"
+        
+        var queryURL = "https://calendarific.com/api/v2/holidays?&api_key=" + APIKey + "&country=US&year=2020";
+
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+        .then(function(result) {
+            
+            // Hard coded with [1] for now. Will want to find a way to check the date and serve up that holiday
+            let holidayName = result.response.holidays[1].name;
+
+            let holidayToday = $("<p>").text(holidayName);
+
+            $("#holidayToday").append(holidayToday);
+
+            console.log(holidayName);
+        });
 
 
+
+    }
+
+    USHolidaysDisplay();
 
 
 
